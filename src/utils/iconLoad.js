a1da307bd6ce29b9d57f0a06a2a92282b62763f6
biggfish3147 <biggfish3147@gmail.com>
2020-06-30 09:59:40 +0800

    与dev同步

---
+++ src/utils/iconLoad.js
@@ -0,0 +1,8 @@
+export default {
+  icon1: require('../assets/icons/scan_r.png'),
+  icon2: require('../assets/icons/report.png'),
+  icon3: require('../assets/icons/more.png'),
+  banner1: require('../assets/images/ilF0001.jpg'),
+  banner2: require('../assets/images/ilF0002.jpg'),
+  banner3: require('../assets/images/ilF1021.jpg'),
+};
