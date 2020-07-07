c2cc3478ea50a0a6fc072733023ef4e38ff07fdc
biggfish3147 <biggfish3147@gmail.com>
2020-07-07 12:13:57 +0800

    添加导航栏右侧按钮

--- src/navigation/Route.js
+++ src/navigation/Route.js
@@ -3,7 +3,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Icon, Card} from 'react-native-elements';
-import {View, StyleSheet, Text, Button} from 'react-native';
+import {StyleSheet, Text, Button} from 'react-native';
 import {BlurView} from 'expo-blur';
 
 import Home from '../screens/Home';
@@ -55,98 +55,90 @@ export function HomeTabs() {
   );
 }
 
-export default function Route() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        // mode="card"
-        headerMode="screen"
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: '#3c78d8',
-            // opacity: 0.8,
-            // height: 60, // 指定导航栏的高度
-          },
-          headerTintColor: '#fff',
-          headerPressColorAndroid: 'skyblue',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-            alignSelf: 'center',
-            flex: 1,
-          },
-          // headerRight: () => (
-          //   <View>
-          //     <Text />
-          //   </View>
-          //   // <Button
-          //   //   onPress={() => alert('This is a button!')}
-          //   //   title="Info"
-          //   //   color="#3c78d8"
-          //   // />
-          // ),
-          headerBackTitle: '返回',
-          // headerShown: false,  //是否显示导航栏（位于页面顶部）
-          // cardStyle: {backgroundColor: 'transparent'}, //栈区卡片样式设计（背景透明的话，上一层卡片可以看到下一层卡片）
-          // cardOverlayEnabled: false,
-
-          // cardStyleInterpolator: ({current: {progress}}) => ({
-          //   cardStyle: {
-          //     opacity: progress.interpolate({
-          //       inputRange: [0, 0.5, 0.9, 1],
-          //       outputRange: [0, 0.25, 0.7, 1],
-          //     }),
-          //   },
-          //   overlayStyle: {
-          //     opacity: progress.interpolate({
-          //       inputRange: [0, 1],
-          //       outputRange: [0, 0.5],
-          //       extrapolate: 'clamp',
-          //     }),
-          //   },
-          // }),
-        }}>
-        <Stack.Screen
-          name="Home"
-          component={HomeTabs}
-          options={{
-            title: 'Welcome to OcrDemo',
-            // headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="Scan"
-          component={Scan}
-          options={{
-            title: '扫描',
-            // headerShown: true,
+export default class Route extends React.Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+          // mode="card"
+          headerMode="screen"
+          screenOptions={{
             headerStyle: {
-              backgroundColor: '#000',
-              opacity: 0.8,
+              backgroundColor: '#3c78d8',
+              // opacity: 0.8,
+              // height: 60, // 指定导航栏的高度
             },
-            headerTransparent: true,
-            headerBackground: () => (
-              <BlurView
-                tint="dark"
-                intensity={100}
-                style={StyleSheet.absoluteFill}
-              />
-            ),
-            headerRight: () => (
-              <View>
-                <Text />
-              </View>
-            ),
-            // headerBackground: '#000',
-            // cardStyle: {backgroundColor: 'transparent'},  //会看到下层卡片
-          }}
-        />
-        <Stack.Screen name="Result" component={Result} />
-        <Stack.Screen name="Doc" component={Doc} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="UserInfo" component={UserInfo} />
-        <Stack.Screen name="ChangePW" component={ChangePW} />
-        <Stack.Screen name="ScanSetting" component={ScanSetting} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+            headerTintColor: '#fff',
+            headerPressColorAndroid: 'skyblue',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              alignSelf: 'center',
+              flex: 1,
+            },
+            // headerRight: () => (
+            //   <View>
+            //     <Text />
+            //   </View>
+            //   // <Button
+            //   //   onPress={() => alert('This is a button!')}
+            //   //   title="Info"
+            //   //   color="#3c78d8"
+            //   // />
+            // ),
+            headerBackTitle: '返回',
+            // headerShown: false,  //是否显示导航栏（位于页面顶部）
+            // cardStyle: {backgroundColor: 'transparent'}, //栈区卡片样式设计（背景透明的话，上一层卡片可以看到下一层卡片）
+            // cardOverlayEnabled: false,
+          }}>
+          <Stack.Screen
+            name="Home"
+            component={HomeTabs}
+            options={{
+              title: 'Welcome to OcrDemo',
+              // headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="Scan"
+            component={Scan}
+            options={{
+              title: '扫描',
+              // headerShown: true,
+              headerStyle: {
+                backgroundColor: '#000',
+                opacity: 0.8,
+              },
+              headerTransparent: true,
+              headerBackground: () => (
+                <BlurView
+                  tint="dark"
+                  intensity={100}
+                  style={StyleSheet.absoluteFill}
+                />
+              ),
+              headerRight: () => (
+                <Button
+                  onPress={() => alert('This is a button!')}
+                  title="设置"
+                  color="transparent"
+                  style={{
+                    // padding: 5,
+                    marginRight: 10,
+                  }}
+                />
+              ),
+              // headerBackground: '#000',
+              // cardStyle: {backgroundColor: 'transparent'},  //会看到下层卡片
+            }}
+          />
+          <Stack.Screen name="Result" component={Result} />
+          <Stack.Screen name="Doc" component={Doc} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="UserInfo" component={UserInfo} />
+          <Stack.Screen name="ChangePW" component={ChangePW} />
+          <Stack.Screen name="ScanSetting" component={ScanSetting} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
